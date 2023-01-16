c3906eca8b2f68bca617e44961724ced2da5f32c
Gladi023 <Gladimar.Villamil@gmail.com>
2023-01-16 15:29:08 -0800

    cities addedd stuled and key updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const API_URL = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={800d72f1dc813b86d0317e29dcbd8408}'
 
 var button = document.querySelector('.btn')
 var inputValue = document.querySelector('.inputValue')
@@ -9,7 +10,7 @@ var temp = document.querySelector('.temp')
 button.addEventListener('click',function() {
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=+inputValue.value+&appid={2ccf4e002e3b58010e13fc9303258cd3}")
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={800d72f1dc813b86d0317e29dcbd8408}}")
   .then(response => response.json())
   .then(data => {
     var nameValue = data['name'];
